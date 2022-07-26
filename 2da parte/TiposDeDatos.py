9cef48a4a976275fffc2214d6badf2c7822eca0e
marvalleo <martinezmarce@gmail.com>
2022-07-26 09:02:31 -0400

    Dia 2 tipos de datos y variables

---
+++ 2da parte/TiposDeDatos.py
@@ -0,0 +1,24 @@
+# str es una cadena de caracteres
+texto: str = "Hola mundo"
+
+# int es un entero
+entero: int = 10
+
+# float es un numero decimal
+flotante: float = 10.5
+
+# bool es un valor booleano
+booleano: bool = True
+
+# list es una lista: tienen un indice
+lista: list = [1, 2, 3, 4, 5]
+
+# tuple es una tupla: no tienen indice, no se puede modificar
+tupla: tuple = (1, 2, 3, 4, 5)
+
+# set es un conjunto: no tiene indice,son pares de valor
+diccionario: dict = {"nombre": "Juan", "apellido": "Perez"}
+
+# set es un conjunto ordenado de elementos unicos, no tiene elementos repetidos
+seteo: set = {1, 2, 3, 4, 5}
+
---
+++ 2da parte/Variables.py
@@ -0,0 +1,6 @@
+nombre = "Juan"
+apellido = "Perez"
+edad = 20
+print("Hola, me llamo " + nombre + " " + apellido + " y tengo " + str(edad) + " años")
+print(nombre, apellido, edad)
+print(type(nombre))
