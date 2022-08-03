aeba97e1ff9d112a224bd6f3854aa4cd1791ed2c
marvalleo <martinezmarce@gmail.com>
2022-08-03 09:13:16 -0400

    Dia 2 operadores

---
+++ 2da parte/Operadores.py
@@ -0,0 +1,13 @@
+x = 6
+y = 2
+z = 7
+print(f"{x} mas {y} es igual a {x + y}")
+print(f"{x} menos {y} es igual a {x - y}")
+print(f"{x} por {y} es igual a {x * y}")
+print(f"{x} dividido {y} es igual a {x / y}")
+
+#el resultado es un entero
+print(f"{x} dividido al piso {y} es igual a {x // y}")
+print(f"{x} modulo {y} es igual a {x % y}")
+print(f"{x} elevado a {y} es igual a {x ** y}")
+print(f"la raiz cuadrada de {x} es igual a {x ** 0.5}")
