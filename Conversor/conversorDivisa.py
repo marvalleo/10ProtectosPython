d2d60af67ab644e54c13d696eb2e1360a3cadb75
marvalleo <martinezmarce@gmail.com>
2022-08-18 06:00:07 -0400

    Conversor USD a CLP

---
+++ Conversor/conversorDivisa.py
@@ -0,0 +1,24 @@
+
+
+import re
+import string
+import requests
+
+monto = input("Introduce el monto CLP -> USD: ")
+
+url = "https://api.apilayer.com/currency_data/convert?to=CLP&from=USD&amount={}".format(monto)
+
+payload = {}
+headers= {
+  "apikey": "UCAXWzbr9OpnbVouDuPpHHpAFHkbKcy2"
+}
+
+response = requests.request("GET", url, headers=headers, data = payload)
+
+status_code = response.status_code
+result = response.text
+found = result[205:215]
+#print(result)
+print("Cantidad en dolares: $",found)
+
+
